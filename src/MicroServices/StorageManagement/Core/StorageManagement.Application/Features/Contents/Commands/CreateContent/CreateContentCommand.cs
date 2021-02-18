541a46445f6919dcf2ed81289ef8e84e6b46907d
mrgrayhat <saeed_ict_msh@yahoo.com>
2021-02-18 20:50:31 +0330

    add create content command and handler

---
+++ src/MicroServices/StorageManagement/Core/StorageManagement.Application/Features/Contents/Commands/CreateContent/CreateContentCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using StorageManagement.Application.DTOs;
+using StorageManagement.Application.Wrappers;
+
+namespace StorageManagement.Application.Features.Contents.Commands.CreateContent
+{
+    public class CreateContentCommand : IRequest<Response<ItemDto>>
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "at least 1 file is required")]
+        public IFormFile File { get; set; }
+        /// <summary>
+        /// a name
+        /// </summary>
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Title is required")]
+        [MaxLength(50, ErrorMessage = "No More than 50 character are allowed")]
+        public string Name { get; set; }
+        public string ContentType { get; private set; }
+        public DateTime Created { get; private set; } = DateTime.UtcNow;
+        public string CreatedBy { get; private set; }
+        public DateTime? LastModified { get; private set; }
+        public string LastModifiedBy { get; private set; }
+        [MaxLength(500, ErrorMessage = "No More that 500 character are allowed")]
+        public string Description { get; set; }
+        public string Tags { get; set; }
+        /// <summary>
+        /// total bytes of data
+        /// </summary>
+        public long Size { get; private set; }
+        /// <summary>
+        /// Unique Hash to protect against invalid/untracked changes
+        /// </summary>
+        public string VerifiedHash { get; private set; }
+
+    }
+}
---
+++ src/MicroServices/StorageManagement/Core/StorageManagement.Application/Features/Contents/Commands/CreateContent/CreateContentCommandHandler.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using StorageManagement.Application.DTOs;
+using StorageManagement.Application.Events;
+using StorageManagement.Application.Exceptions;
+using StorageManagement.Application.Extensions;
+using StorageManagement.Application.Interfaces;
+using StorageManagement.Application.Interfaces.Repositories;
+using StorageManagement.Application.Wrappers;
+using StorageManagement.Domain.Entities;
+
+namespace StorageManagement.Application.Features.Contents.Commands.CreateContent
+{
+    public class CreateContentCommandHandler : IRequestHandler<CreateContentCommand, Response<ItemDto>>
+    {
+        private readonly IStorageRepositoryAsync _storageRepository;
+        private readonly IStorageFileSystemProvider _storageFileSystemProvider;
+        private readonly IMapper _mapper;
+        private readonly IHttpContextAccessor _httpContext;
+        private readonly IMediator _mediator;
+        private readonly ILogger<CreateContentCommandHandler> _logger;
+
+        public CreateContentCommandHandler(IStorageRepositoryAsync storageRepository, IMapper mapper, IMediator mediator, IStorageFileSystemProvider storageFileSystemProvider, ILogger<CreateContentCommandHandler> logger, IHttpContextAccessor httpContext)
+        {
+            _httpContext = httpContext ?? throw new ArgumentNullException($"{nameof(httpContext)} is null");
+            _logger = logger ?? throw new ArgumentNullException($"{nameof(logger)} is null");
+            _storageFileSystemProvider = storageFileSystemProvider ?? throw new ArgumentNullException($"{nameof(storageFileSystemProvider)} is null"); ;
+            _storageRepository = storageRepository ?? throw new ArgumentNullException($"{nameof(storageRepository)} is null"); ;
+            _mapper = mapper ?? throw new ArgumentNullException($"{nameof(mapper)} is null"); ;
+            _mediator = mediator ?? throw new ArgumentNullException($"{nameof(mediator)} is null");
+        }
+
+        public async Task<Response<ItemDto>> Handle(CreateContentCommand request, CancellationToken cancellationToken)
+        {
+            Item item = _mapper.Map<Item>(request);
+            ItemDto result;
+            try
+            {
+                item.Name = request.Name ?? Path.GetFileNameWithoutExtension(request.File.FileName);
+                item.ContentType = request.File.GetContentType();
+                item.CreatedBy = _httpContext.HttpContext.User.Identity.Name;
+                item.Size = request.File.Length;
+                item.VerifiedHash = await request.File
+                    .CalculateMD5FileHashAsync(cancellationToken);
+                item.Url = await _storageFileSystemProvider
+                    .StoreAsync(request.File, cancellationToken).ConfigureAwait(false);
+                _logger.LogInformation("storage item {Name} successfully stored in {Url}", item.Name, item.Url);
+
+                result = _mapper.Map<ItemDto>(await _storageRepository.AddAsync(item));
+                // Raising new content created Event ...
+                await _mediator.Publish(new ContentCreatedEvent(DateTime.Now, $"{item.CreatedBy}, {_httpContext.HttpContext.Connection.RemoteIpAddress}", item.VerifiedHash, item.Url), cancellationToken);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("failed to store item {Name}, {ex}", item.Name, ex);
+                throw new ApiException(ex.Message);
+            }
+            return new Response<ItemDto>(result);
+        }
+    }
+}
