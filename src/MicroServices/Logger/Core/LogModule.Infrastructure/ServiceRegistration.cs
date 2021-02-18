6919cde2e2eafc412648249c256b4248eca2d662
mrgrayhat <saeed_ict_msh@yahoo.com>
2021-02-18 22:16:12 +0330

    add logger open api def and update service registration

--- src/MicroServices/Logger/Core/LogModule.Infrastructure/ServiceRegistration.cs
+++ src/MicroServices/Logger/Core/LogModule.Infrastructure/ServiceRegistration.cs
@@ -1,16 +1,29 @@
-﻿using LogModule.Application.Interfaces;
+﻿using System.Linq;
+using LogModule.Application.Interfaces;
 using LogModule.Application.Interfaces.Repositories;
 using LogModule.Domain.Settings;
 using LogModule.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Options;
+using NSwag;
+using NSwag.Generation.Processors.Security;
 
 namespace LogModule.Infrastructure
 {
     public static class ServiceRegistration
     {
+        public static IServiceCollection AddLoggerInfrastructures(this IServiceCollection services, IConfiguration configuration)
+        {
+            services
+                .AddCustomUi()
+                .AddLogModulePersistenceInfrastructure(configuration);
+
+            return services;
+        }
+
         /// <summary>
         /// Configure Log Database. <inheritdoc cref="IConfiguration"/>
         /// AddLogModule Service. <inheritdoc cref="IServiceCollection"/>
@@ -28,6 +41,39 @@ namespace LogModule.Infrastructure
             services.AddSingleton(typeof(IGenericRepositoryAsync<>), typeof(GenericRepositoryAsync<>));
             services.AddSingleton<ILogRepositoryAsync, LogRepositoryAsync>();
             #endregion
+
+            return services;
+        }
+
+        private static IServiceCollection AddCustomUi(this IServiceCollection services)
+        {
+            services.AddOpenApiDocument(configure =>
+            {
+                configure.Title = "Blog API";
+                configure.AddSecurity("JWT", Enumerable.Empty<string>(),
+                    new OpenApiSecurityScheme
+                    {
+                        Type = OpenApiSecuritySchemeType.ApiKey,
+                        Name = "Authorization",
+                        In = OpenApiSecurityApiKeyLocation.Header,
+                        Description = "Type into the textbox: Bearer {your JWT token}."
+                    });
+
+                configure.OperationProcessors.Add(
+                    new AspNetCoreOperationSecurityScopeProcessor("JWT"));
+            });
+
+            // Customise default API behaviour
+            services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.SuppressModelStateInvalidFilter = true;
+            });
+
+            //var controllerWithViews = services.AddControllersWithViews();
+            services.AddControllers().AddJsonOptions((opt) =>
+            {
+                opt.JsonSerializerOptions.WriteIndented = true;
+            });
 
             return services;
         }
