43a693d800a004d12171579098782f03f4dc989c
mrgrayhat <saeed_ict_msh@yahoo.com>
2021-02-18 20:42:49 +0330

    add memory health check metric

---
+++ src/MicroServices/StorageManagement/Core/StorageManagement.Infrastraucture/Common/HealthChecks/SystemMemoryHealthcheck.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace StorageManagement.Infrastraucture.Common.HealthChecks
+{
+    public class SystemMemoryHealthcheck : IHealthCheck
+    {
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            var client = new MemoryMetricsClient();
+            var metrics = client.GetMetrics();
+            var percentUsed = 100 * metrics.Used / metrics.Total;
+
+            var status = HealthStatus.Healthy;
+            if (percentUsed > 80)
+            {
+                status = HealthStatus.Degraded;
+            }
+            if (percentUsed > 90)
+            {
+                status = HealthStatus.Unhealthy;
+            }
+
+            var data = new Dictionary<string, object>
+            {
+                { "Total", metrics.Total },
+                { "Used", metrics.Used },
+                { "Free", metrics.Free }
+            };
+
+            var result = new HealthCheckResult(status, null, null, data);
+
+            return await Task.FromResult(result);
+        }
+    }
+}
