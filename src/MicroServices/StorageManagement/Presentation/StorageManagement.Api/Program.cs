e1e641183cec20239becf22aeb945bd41ee58ff5
mrgrayhat <saeed_ict_msh@yahoo.com>
2021-02-18 22:14:05 +0330

    add storageAgent cleanup task at api startup, for clean deleted files from fs but not from db index

--- src/MicroServices/StorageManagement/Presentation/StorageManagement.Api/Program.cs
+++ src/MicroServices/StorageManagement/Presentation/StorageManagement.Api/Program.cs
@@ -13,12 +13,50 @@ namespace StorageManagement.Api
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             IConfigurationRoot configuration = new ConfigurationBuilder()
                 .AddJsonFile("appsettings.json", false, true)
                 .Build();
             IHost host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                IServiceProvider services = scope.ServiceProvider;
+                ILogger<Program> logger = services.GetRequiredService<ILogger<Program>>();
+                try
+                {
+                    logger.LogInformation("Press x to cancel StorageAgent task");
+                    var tokenSource = new CancellationTokenSource();
+                    var cancellationToken = tokenSource.Token;
+                    IStorageAgent storageAgent = services.GetRequiredService<IStorageAgent>();
+                    //await storageAgent.CleanupDatabase(cancellationToken);
+                    await Task.Run(() =>
+                    {
+                        Task<Task> task = storageAgent.CleanupDatabase(cancellationToken);
+                        while (!task.IsCompleted)
+                        {
+                            if (Console.ReadKey().Key == ConsoleKey.X)
+                            {
+                                tokenSource.Cancel();
+                                logger.LogWarning("x key pressed, Operation Cancelled.");
+                                break;
+                            }
+                        }
+                        return Task.CompletedTask;
+                    }, cancellationToken);
+
+
+                }
+                catch (Exception ex)
+                {
+                    logger.LogCritical("Error executing/completeing Storage Agent Service" + ex);
+                    //Log.CloseAndFlush();
+                    //await host.StopAsync();
+                }
+            }
+
+
             host.Run();
         }
 
